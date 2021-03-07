5edfa41139ce4e8aed13f38dd11c27295d62e15c
kyychiu <kyychiu@hkma.gov.hk>
2021-03-07 19:22:56 +0800

    sql schema add material id; update ER diagram

--- schema_basic.sql
+++ schema_basic.sql
@@ -53,6 +53,7 @@ CREATE TABLE `Lesson` (
 
 
 CREATE TABLE `CourseMaterial` (
+  `material_id` varchar(100) NOT NULL,
   `course_code` varchar(50) NOT NULL,
   `material_name` varchar(50) NOT NULL,
   `material_link` varchar(100) NOT NULL,
