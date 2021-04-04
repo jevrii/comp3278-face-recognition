f53335b6ca1786f04f16d23fb2016fabe67223b7
hmth413 <79845646+hmth413@users.noreply.github.com>
2021-04-05 02:28:40 +0800

    Update of GUI

--- Welcome_GUI.py
+++ Welcome_GUI.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Welcome.ui'
+# Form implementation generated from reading ui file 'Welcome_GUI.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.2
 #
@@ -14,28 +14,39 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(400, 250)
-        Form.setMaximumSize(QtCore.QSize(400, 250))
+        Form.resize(800, 500)
+        Form.setMinimumSize(QtCore.QSize(0, 0))
         self.OkayButton_Welcome = QtWidgets.QPushButton(Form)
-        self.OkayButton_Welcome.setGeometry(QtCore.QRect(100, 180, 75, 23))
+        self.OkayButton_Welcome.setGeometry(QtCore.QRect(200, 410, 100, 50))
         self.OkayButton_Welcome.setObjectName("OkayButton_Welcome")
         self.CancelButton_Welcome = QtWidgets.QPushButton(Form)
-        self.CancelButton_Welcome.setGeometry(QtCore.QRect(225, 180, 75, 23))
+        self.CancelButton_Welcome.setGeometry(QtCore.QRect(490, 410, 100, 50))
         self.CancelButton_Welcome.setObjectName("CancelButton_Welcome")
         self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(155, 40, 90, 21))
+        self.label.setGeometry(QtCore.QRect(0, 30, 800, 100))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
         font = QtGui.QFont()
-        font.setPointSize(15)
+        font.setPointSize(35)
         self.label.setFont(font)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setGeometry(QtCore.QRect(75, 80, 250, 71))
+        self.label_2.setGeometry(QtCore.QRect(0, 120, 800, 250))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy)
         font = QtGui.QFont()
-        font.setPointSize(13)
+        font.setPointSize(30)
         self.label_2.setFont(font)
         self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setWordWrap(True)
+        self.label_2.setOpenExternalLinks(False)
         self.label_2.setObjectName("label_2")
 
         self.retranslateUi(Form)
@@ -43,8 +54,8 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Form", "Welcome"))
         self.OkayButton_Welcome.setText(_translate("Form", "Okay"))
         self.CancelButton_Welcome.setText(_translate("Form", "Cancel"))
         self.label.setText(_translate("Form", "Welcome!"))
-        self.label_2.setText(_translate("Form", "Please make sure your face is not obstructed, then press the \"Okay\" button."))
+        self.label_2.setText(_translate("Form", "Please show your face."))
