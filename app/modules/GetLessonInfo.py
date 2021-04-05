bf15f714f7475663f49968562ed30f7f6fbe3948
jevrii <jevri@connect.hku.hk>
2021-04-05 14:26:49 +0800

    moved app stuff, basic code structure

---
+++ app/modules/GetLessonInfo.py
@@ -0,0 +1,18 @@
+import mysql.connector
+# import password
+
+class GetLessonInfo:
+    def __init__(self):
+        pass
+        # self.conn = mysql.connector.connect(host="localhost", user="root", passwd=password.secret, database="face_recognition")
+
+    def get_info(self, lesson_id):
+        # TODO: implementation, SQL
+        d = {}
+        # course information, classroom address, teacher’s message, links of Zoom
+        # tutorial/lecture notes, other course materials and so on and so forth will be presented in the GUI.
+
+        return d
+
+    def test(self):
+        print("hi")
