ebecb2b7e5a213fa5eb57b9db654938d2ca1757b
Jeffrey Lee <jevri@connect.hku.hk>
2021-04-09 18:45:16 +0800

    can display camera in welcome ui

--- app/Welcome_code.py
+++ app/Welcome_code.py
@@ -3,20 +3,118 @@ from PyQt5 import QtCore, QtWidgets
 from Welcome_GUI import Ui_Form
 from Student_Information_code import InfoWindow
 
+# USB camera display using PyQt and OpenCV, from iosoft.blog
+# Copyright (c) Jeremy P Bentham 2019
+# Please credit iosoft.blog if you use the information or software in it
+
+VERSION = "Cam_display v0.10"
+
+import sys, time, threading, cv2
+try:
+    from PyQt5.QtCore import Qt
+    pyqt5 = True
+except:
+    pyqt5 = False
+if pyqt5:
+    from PyQt5.QtCore import QTimer, QPoint, pyqtSignal
+    from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QLabel
+    from PyQt5.QtWidgets import QWidget, QAction, QVBoxLayout, QHBoxLayout
+    from PyQt5.QtGui import QFont, QPainter, QImage, QTextCursor
+else:
+    from PyQt4.QtCore import Qt, pyqtSignal, QTimer, QPoint
+    from PyQt4.QtGui import QApplication, QMainWindow, QTextEdit, QLabel
+    from PyQt4.QtGui import QWidget, QAction, QVBoxLayout, QHBoxLayout
+    from PyQt4.QtGui import QFont, QPainter, QImage, QTextCursor
+try:
+    import Queue as Queue
+except:
+    import queue as Queue
+
+IMG_SIZE    = 640,480          # 640,480 or 1280,720 or 1920,1080
+IMG_FORMAT  = QImage.Format_RGB888
+DISP_SCALE  = 2                # Scaling factor for display image
+DISP_MSEC   = 50                # Delay between display cycles
+CAP_API     = cv2.CAP_ANY       # API: CAP_ANY or CAP_DSHOW etc...
+EXPOSURE    = 0                 # Zero for automatic exposure
+TEXT_FONT   = QFont("Courier", 10)
+
+camera_num  = 1                 # Default camera (first in list)
+image_queue = Queue.Queue()     # Queue to hold images
+capturing   = True              # Flag to indicate capturing
+
+# Grab images from the camera (separate thread)
+def grab_images(cam_num, queue):
+    cap = cv2.VideoCapture(cam_num-1 + CAP_API)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, IMG_SIZE[0])
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, IMG_SIZE[1])
+    if EXPOSURE:
+        cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
+        cap.set(cv2.CAP_PROP_EXPOSURE, EXPOSURE)
+    else:
+        cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+    while capturing:
+        if cap.grab():
+            retval, image = cap.retrieve(0)
+            if image is not None and queue.qsize() < 2:
+                queue.put(image)
+            else:
+                time.sleep(DISP_MSEC / 1000.0)
+        else:
+            print("Error: can't grab camera image")
+            break
+    cap.release()
+
+
 class WelcomeWindow(QtWidgets.QMainWindow, Ui_Form):
     def __init__(self):
         super(WelcomeWindow, self).__init__()
         self.setupUi(self)
         self.OkayButton_Welcome.clicked.connect(self.faces)
-        self.CancelButton_Welcome.clicked.connect(self.close)
         self._new_window = None
     def faces(self):
         self._new_window = InfoWindow('177013', '2020-01-13 15:30')
         self._new_window.show()
+    
+    # Start image capture & display
+    def start(self):
+        self.timer = QTimer(self)           # Timer to trigger display
+        self.timer.timeout.connect(lambda: 
+                    self.show_image(image_queue, self.camera_display, DISP_SCALE))
+        self.timer.start(DISP_MSEC)         
+        self.capture_thread = threading.Thread(target=grab_images, 
+                    args=(camera_num, image_queue))
+        self.capture_thread.start()         # Thread to grab images
+
+    # Fetch camera image from queue, and display it
+    def show_image(self, imageq, display, scale):
+        if not imageq.empty():
+            image = imageq.get()
+            if image is not None and len(image) > 0:
+                img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                self.display_image(img, display, scale)
+
+    # Display an image, reduce size if required
+    def display_image(self, img, display, scale=1):
+        disp_size = img.shape[1]//scale, img.shape[0]//scale
+        disp_bpl = disp_size[0] * 3
+        if scale > 1:
+            img = cv2.resize(img, disp_size, 
+                             interpolation=cv2.INTER_CUBIC)
+        qimg = QImage(img.data, disp_size[0], disp_size[1], 
+                      disp_bpl, IMG_FORMAT)
+        display.setImage(qimg)
+    
+    # Window is closing: stop video capture
+    def closeEvent(self, event):
+        global capturing
+        capturing = False
+        self.capture_thread.join()
 
 if __name__ == '__main__':
+    camera_num = 1
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     app = QtWidgets.QApplication(sys.argv)
     welcome_window = WelcomeWindow()
     welcome_window.show()
+    welcome_window.start()
     sys.exit(app.exec_())
