41d2233f4184181907f43071c4ae280a55e628b1
jevrii <jevri@connect.hku.hk>
2021-04-09 15:33:23 +0800

    rename

---
+++ app/Student_Information_code.py
@@ -0,0 +1,35 @@
+import sys
+import smtplib
+import email
+from PyQt5 import QtCore, QtWidgets
+from Student_Information_GUI import Ui_Form
+
+class InfoWindow(QtWidgets.QMainWindow, Ui_Form):
+    def __init__(self):
+        super(InfoWindow, self).__init__()
+        self.setupUi(self)
+        self.okay_info.clicked.connect(self.email)
+        self.cancel_info.clicked.connect(self.close)
+    def email(self):
+        # if student has class within one hour:
+        #   self.textBrowser.append()
+        # else:
+        #   self.textBrowser.append()
+        if self.sendtoemail.isChecked():
+            # sent_msg = email.message.EmailMessage()
+            # sent_msg.set_content()
+            # sent_msg['Subject'] = 
+            # sent_msg['From'] =
+            # sent_msg['To'] =
+            # smtp_server = smtplib.SMTP('mail.cs.hku.hk')
+            # smtp_server.send_message(sent_msg)
+            # smtp_server.quit()
+            pass
+        self.close()
+
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+app = QtWidgets.QApplication(sys.argv)
+info_window = InfoWindow()
+info_window.show()
+sys.exit(app.exec_())
+        
