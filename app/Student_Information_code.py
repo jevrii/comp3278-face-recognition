0e3dd17622954296cfb8c3b3bab9778caa1db6a8
Jeffrey Lee <jevri@connect.hku.hk>
2021-04-10 22:09:57 +0800

    fix start/end datetime

--- app/Student_Information_code.py
+++ app/Student_Information_code.py
@@ -16,7 +16,7 @@ class LessonWidget(QtWidgets.QWidget):
         label_course.setFixedHeight(20)
         label_venue = QtWidgets.QLabel(f"{venue}")
         label_venue.setFixedHeight(20)
-        label_datetime = QtWidgets.QLabel(f"datetime 9/3/2020 15:30 - 16:20")
+        label_datetime = QtWidgets.QLabel(f"{start_datetime.day}/{start_datetime.month}/{start_datetime.year} {start_datetime.hour}:{start_datetime.minute} - {end_datetime.hour}:{end_datetime.minute}")
         label_datetime.setFixedHeight(20)
 
         widgetLayout = QtWidgets.QVBoxLayout()
@@ -25,6 +25,8 @@ class LessonWidget(QtWidgets.QWidget):
         widgetLayout.addWidget(label_datetime)
 
         self.setLayout(widgetLayout)
+
+
 
 class InfoWindow(QtWidgets.QMainWindow, Ui_Form):
     def __init__(self, student_id, timestamp):
@@ -69,7 +71,6 @@ class InfoWindow(QtWidgets.QMainWindow, Ui_Form):
             status1 = "You have lessons in 1 hour.\n"
             status2 = "Class material:\n"
             lesson_info = db_backend.GetLessonInfo().get_info(student_info['lessons'][0])
-            text = str(lesson_info)
         else:
             status1 = "You do not have lessons in 1 hour.\n"
             status2 = "Your upcoming classes:\n"
@@ -79,15 +80,13 @@ class InfoWindow(QtWidgets.QMainWindow, Ui_Form):
                 itemN = QtWidgets.QListWidgetItem()
                 widget = LessonWidget(course_name=lesson["course_name"], course_code=lesson["course_name"],
                                       venue=lesson["venue"], type=lesson["type"],
-                                      start_datetime=lesson["start_datetime"], end_datetime=lesson["start_datetime"])
+                                      start_datetime=lesson["start_datetime"], end_datetime=lesson["end_datetime"])
                 itemN.setSizeHint(widget.sizeHint())
                 self.listWidget.addItem(itemN)
                 self.listWidget.setItemWidget(itemN, widget)
 
         self.label_2.setText(_translate("Form", f"<html><head/><body><p><span style=\" font-size:12pt;\">{status1}</span></p></body></html>"))
         self.label_3.setText(_translate("Form", f"<html><head/><body><p><span style=\" font-size:12pt;\">{status2}</span></p></body></html>"))
-
-        # self.message = text
 
 if __name__ == '__main__':
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
